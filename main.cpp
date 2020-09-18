151cc9eae0067e28a99f04f2ae47780126819837
nasdda <36829770+nasdda@users.noreply.github.com>
2020-09-18 15:43:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 // debug
 template < class c > struct rge { c b, e; };
-template < class c > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+template < class c > rge<c> range(c i, c j) { return rge<c> {i, j}; }
 template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
 template < class c > char dud(...);
 struct debug {
 	~debug() { cerr << endl; }
 	template < class c > typename \
-	enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+	enable_if < sizeof dud<c>(0) != 1, debug& >::type operator<<(c i) {
 		cerr << boolalpha << i;
 		return * this;
 	}
 	template < class c > typename \
 	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
-		return * this << range(begin(i), end(i)); 
+		return * this << range(begin(i), end(i));
 	}
 	template < class c, class b > debug & operator << (pair < b, c > d) {
 		return * this << "(" << d.first << ", " << d.second << ")";
@@ -28,12 +28,11 @@ struct debug {
 	}
 };
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define DEBUG(x) do { cerr << #x << ": " << x << " ";} while (0)
+#define DEBUG(x) cerr << #x << ": " << x << endl;
 // macros
 #define FASTIO() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define rep(n) for(int _ = 0; _ < n; _++)
-#define forn(i, n) for(int i = 0; i < n; i++)
-#define forab(i, a, b) for(int i = a; i < b; i++)
+#define forn(i, a, b, x) for(int i = a; i != b; i += x)
 #define iter(it, target) for(auto it = target.begin(); it != target.end(); it++)
 #define all(x) x.begin(), x.end()
 #define sz(x) ((int) x.size())
