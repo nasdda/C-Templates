dc1057d7376ddd3cab597f72abd63b6abe6e6092
nasdda <36829770+nasdda@users.noreply.github.com>
2020-09-17 22:45:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// debug
+template < class c > struct rge { c b, e; };
+template < class c > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+struct debug {
+	~debug() { cerr << endl; }
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+		cerr << boolalpha << i;
+		return * this;
+	}
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+		return * this << range(begin(i), end(i)); 
+	}
+	template < class c, class b > debug & operator << (pair < b, c > d) {
+		return * this << "(" << d.first << ", " << d.second << ")";
+	}
+	template < class c > debug & operator <<(rge<c> d) {
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		return * this << "]";
+	}
+};
+#define DEBUG(x) do { cerr << #x << ": " << x << " ";} while (0)
 // macros
 #define FASTIO() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-#define DEBUG(x) do { cerr << #x << ": " << x << " ";} while (0)
-#define DEBUGV(v) cerr << #v << ": "; for(int __ = 0; __ < v.size()-1; __++) \
-	{cout << v[__] << ", ";} cout << v.back() << endl;
 #define rep(n) for(int _ = 0; _ < n; _++)
 #define forn(i, n) for(int i = 0; i < n; i++)
 #define forab(i, a, b) for(int i = a; i < b; i++)
 #define iter(it, target) for(auto it = target.begin(); it != target.end(); it++)
 #define all(x) x.begin(), x.end()
-#define sz(x) (int) x.size()
+#define sz(x) ((int) x.size())
 #define um unordered_map
 #define us unordered_set
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 // constants
 const long MOD = 1000000007;
 
