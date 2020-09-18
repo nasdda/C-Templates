39dd5fa774e7b8fc30b8bdd849a99d1db669cea8
nasdda <36829770+nasdda@users.noreply.github.com>
2020-09-17 20:36:07 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// macros
+#define FASTIO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define DEBUG(x) do { std::cerr << #x << ": " << x << " ";} while (0)
+#define DEBUGV(v) std::cerr << #v << ": "; for(int __ = 0; __ < v.size(); __++) \
+	{if(__ < v.size()-1) std::cout << v[__] << ", "; else std::cout << v[__] << endl;}
+#define rep(n) for(int _ = 0; _ < n; _++)
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define forab(i, a, b) for(int i = a; i < b; i++)
+#define iter(it, target) for(auto it = target.begin(); it != target.end(); it++)
+#define um unordered_map
+#define us unordered_set
+// constants
+const long MOD = 1000000007;
+
+
+int main() {
+
+}
