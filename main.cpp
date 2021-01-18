f28efada8f4838035987a1dd61612a8279af656d
nasdda <36829770+nasdda@users.noreply.github.com>
2021-01-17 18:19:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
+// input
+template<class A> void read(vector<A>& v) {
+	for (auto& x : v)
+		cin >> x;
+}
 // debug
-template < class c > struct rge { c b, e; };
-template < class c > rge<c> range(c i, c j) { return rge<c> {i, j}; }
-template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
-template < class c > char dud(...);
+template <class c> struct rge { c b, e; };
+template <class c> rge<c> range(c i, c j) { return rge<c> {i, j}; }
+template <class c> auto dud(c* x) -> decltype(cout << *x, 0);
+template <class c> char dud(...);
 struct debug {
-	~debug() { cerr << endl; }
+	~debug() { cout << endl; }
 	template < class c > typename \
 	enable_if < sizeof dud<c>(0) != 1, debug& >::type operator<<(c i) {
-		cerr << boolalpha << i;
+		cout << boolalpha << i;
 		return * this;
 	}
-	template < class c > typename \
+	template <class c> typename \
 	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
 		return * this << range(begin(i), end(i));
 	}
-	template < class c, class b > debug & operator << (pair < b, c > d) {
+	template <class c, class b> debug & operator << (pair < b, c > d) {
 		return * this << "(" << d.first << ", " << d.second << ")";
 	}
-	template < class c > debug & operator <<(rge<c> d) {
+	template <class c> debug & operator <<(rge<c> d) {
 		*this << "[";
 		for (auto it = d.b; it != d.e; ++it)
 			*this << ", " + 2 * (it == d.b) << *it;
@@ -31,15 +36,18 @@ struct debug {
 #define DEBUG(x) cerr << #x << ": " << x << endl;
 // macros
 #define FASTIO() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-#define rep(n) for(int _ = 0; _ < n; _++)
-#define forn(i, a, b, x) for(int i = a; i != b; i += x)
-#define iter(it, target) for(auto it = target.begin(); it != target.end(); it++)
-#define all(x) x.begin(), x.end()
-#define sz(x) ((int) x.size())
+#define REP(n) for(int _ = 0; _ < n; _++)
+#define ITER(it, target) for(auto it = target.begin(); it != target.end(); it++)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) ((int) x.size())
+#define LEN(x) ((int) x.length())
+#define IN(s, x) (s.find(x) != s.end())
 #define um unordered_map
 #define us unordered_set
+#define ll long long
 // constants
 const long MOD = 1000000007;
+
 
 int main() {
 
