8edf060dd1f2227816b495b1c4e21a9a585e96d0
nasdda <36829770+nasdda@users.noreply.github.com>
2021-01-19 02:52:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,24 +13,10 @@ template <class c> auto dud(c* x) -> decltype(cout << *x, 0);
 template <class c> char dud(...);
 struct debug {
 	~debug() { cout << endl; }
-	template < class c > typename \
-	enable_if < sizeof dud<c>(0) != 1, debug& >::type operator<<(c i) {
-		cout << boolalpha << i;
-		return * this;
-	}
-	template <class c> typename \
-	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
-		return * this << range(begin(i), end(i));
-	}
-	template <class c, class b> debug & operator << (pair < b, c > d) {
-		return * this << "(" << d.first << ", " << d.second << ")";
-	}
-	template <class c> debug & operator <<(rge<c> d) {
-		*this << "[";
-		for (auto it = d.b; it != d.e; ++it)
-			*this << ", " + 2 * (it == d.b) << *it;
-		return * this << "]";
-	}
+	template < class c > typename enable_if < sizeof dud<c>(0) != 1, debug& >::type operator<<(c i) { cout << boolalpha << i; return * this; }
+	template <class c> typename enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) { return * this << range(begin(i), end(i)); }
+	template <class c, class b> debug & operator << (pair < b, c > d) { return * this << "(" << d.first << ", " << d.second << ")"; }
+	template <class c> debug & operator <<(rge<c> d) { *this << "["; for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it; return * this << "]";}
 };
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 #define DEBUG(x) cerr << #x << ": " << x << endl;
